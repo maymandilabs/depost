dc122daa228db3a33ed2a1f5f621e06a8200871d
Mahdi M. <45321267+mahdimaymandi@users.noreply.github.com>
2025-08-19 08:29:40 -0400

    Create config.js
    Added config file

---
+++ config.js
@@ -0,0 +1,5 @@
+const HOST = "arweave.net";
+const PORT = 443;
+const PROTOCOL = "HTTPS";
+const POST_LINK_BASE = "https://arweave.net/";
+const BLOCK_LINK_BASE = "https://viewblock.io/arweave/tx/";
